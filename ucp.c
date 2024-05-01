891f255955455daa2e7e1f35e009b80d27702d8e
NanoCode <119601468+nanocodium@users.noreply.github.com>
2024-05-01 01:25:07 +0000

    modified so running the program over again after running an option is no longer neede

--- ucp.c
+++ ucp.c
@@ -2,28 +2,30 @@
 #include <stdlib.h>
 int main (){
 	int choice;
+main:
 	system("clear");
 	printf("Nano Debian Control Panel 0.1\n");
-	printf("\n1. Get dependencies\n2. Download minecraft 1.19.3 server\n3. Edit eula.txt\n4. run server with 4gb of ram\n5. run server with 8gb of ram\n");
+	printf("\n1. Get dependencies\n2. Download minecraft 1.19.3 server\n3. Edit eula.txt\n4. run server with 4gb of ram\n5. run server with 8gb of ram\n6. exit\n");
 	printf("Type a number and hit enter: ");
 	scanf(" %d", &choice);	
 	switch (choice) {
 		case 1:
 		system("sudo apt install openjdk-17-jre");
-		int choice = 0;
-		break;
+		goto main;
 		case 2:
 		system("mkdir mc_server && cd mc_server && wget https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar ");
-		break;
+		goto main;
 		case 3:
 		system("cd mc_server && java -jar server.jar nogui");
 		system("cd mc_server && nano ./eula.txt");
-		break;
+		goto main;
 		case 4:
 		system("cd mc_server && java -Xms4G -Xmx4G -jar server.jar");
-		break;
+		goto main;
 		case 5:
 		system("cd mc_server && java -Xms8G -Xmx8G -jar server.jar");
+		goto main;
+		case 6:
 		break;
 	}
 	return 0;	
